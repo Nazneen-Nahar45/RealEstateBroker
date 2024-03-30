aaeebf2667c2168de26c241a9aa49578b4621b77
Nazneen-Nahar45 <nazneen.nahar145@gmail.com>
2024-03-30 23:40:56 +0600

    user login

--- realestate/views.py
+++ realestate/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login as auth_login
+from django.contrib import messages
+from django.contrib.sessions.models import  Session
 # Create your views here.
 
 def buy(request):
@@ -36,9 +39,38 @@ def firstpage(request):
     return render(request,template_name='Admin_User.html')
 
 def user_sign(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.create_user(username=username , email=email , password = password)
+
+        request.session['signup_username']= username
+        request.session['signup_email'] = email
+        request.session['signup_password'] = password
+
+        user = authenticate(request,username=username , password = password)
+
+        if user is not None :
+            auth_login(request,user)
+            messages.success(request,'Successfulyy signed up!')
+            return redirect('user_login')
     return render(request,template_name='User_Signup.html')
 
+
 def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None :
+            auth_login(request,user)
+            messages.success(request,'Successfulyy Logged In!')
+            return redirect('user_home')
+
     return render(request,template_name='User_Login.html')
 
 def user_home(request):
