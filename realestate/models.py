b3bb5de6f1d7fd002ddde41818d09e233c09347f
SamiulUtshaw <gamerlucifer78@gmail.com>
2024-04-04 00:39:55 +0600

    create models

--- realestate/models.py
+++ realestate/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Agent(models.Model):
+    name = models.CharField(max_length=160)
+    password = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+
+
+class Buy(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    house_code = models.CharField(max_length=20)
+    location = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    area = models.DecimalField(max_digits=10, decimal_places=2)
+    image= models.ImageField(upload_to='images/',blank=True, null=True, default='images/default.jpg')
+
+
+
+class Rent(models.Model):
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+    house_code = models.CharField(max_length=20)
+    location = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    area = models.DecimalField(max_digits=10, decimal_places=2)
+    image= models.ImageField(upload_to='images/',blank=True, null=True, default='images/default.jpg')
+
+
+
+class Sell(models.Model):
+    
+    house_code = models.CharField(max_length=20)
+    location = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    area = models.DecimalField(max_digits=10, decimal_places=2)
+    image= models.ImageField(upload_to='images/',blank=True, null=True, default='images/default.jpg')
+    
+    def __str__(self):
+        return self.house_code 
