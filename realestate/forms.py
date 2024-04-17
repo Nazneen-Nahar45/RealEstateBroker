a4fa5694d3096a0a58d86439a15ba8e3020306a9
SamiulUtshaw <gamerlucifer78@gmail.com>
2024-04-17 20:16:11 +0600

    forms.py edited

--- realestate/forms.py
+++ realestate/forms.py
@@ -1,7 +1,16 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, widgets
+from django import forms
 from .models import *
 
-class Sellform(ModelForm):
+class SellForm(forms.ModelForm):
     class Meta:
         model = Sell
         fields = '__all__'
+        widgets = {
+            'house_code': forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 500px;'}),
+            'location': forms.TextInput(attrs={'class': 'form-control', 'style': 'width: 500px;'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 500px;'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'style': 'height: 90px; width: 500px'}),
+            'area': forms.NumberInput(attrs={'class': 'form-control', 'style': 'width: 500px;'}),
+            'image': forms.FileInput(attrs={'class': 'form-control', 'style': 'width: 500px;'}),
+        }
